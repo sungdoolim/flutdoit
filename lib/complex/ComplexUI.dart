71639091367dafa7a6c5299e2792669f670642a9
sungdoolim <sungdoolim@naver.com>
2021-11-13 03:16:05 +0900

    complex ui

--- lib/complex/ComplexUI.dart
+++ lib/complex/ComplexUI.dart
@@ -1,64 +1,68 @@
 import 'package:flutter/material.dart';
 
 import 'Page1.dart';
+import 'Page2.dart';
+import 'Page3.dart';
+
 void main() {
   runApp(ComplexUI());
 }
-class ComplexUI extends StatelessWidget{
+
+class ComplexUI extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title : 'Flut complex',
-      theme:ThemeData(
+      title: 'Flut complex',
+      theme: ThemeData(
         primarySwatch: Colors.lightBlue,
       ),
-      home:MyHomePage(),
-
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget{
-
-
+class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() {
     return _MyHomePageState();
   }
 }
-class _MyHomePageState extends State<MyHomePage>{
 
-  var _index=0;
-  var _pages=[Page1(),Page2(),Page3()];
+class _MyHomePageState extends State<MyHomePage> {
+  var _index = 0;
+  var _pages = [Page1(), Page2(), Page3()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        title:Text("복잡 ui",style:TextStyle(color:Colors.black)),
+        title: Text("복잡 ui", style: TextStyle(color: Colors.black)),
         actions: [
           IconButton(
-            icon:Icon(Icons.add, color:Colors.black),
-            onPressed: (){ },
+            icon: Icon(Icons.add, color: Colors.black),
+            onPressed: () {},
           )
         ],
       ),
-      body:_pages[_index],
+      body: Builder(builder: (context){
+        return _pages[_index];
+    }),
+
+
       bottomNavigationBar: BottomNavigationBar(
-        onTap:(index){
+        onTap: (index) {
           setState(() {
-            _index=index;
+            _index = index;
           });
         },
         currentIndex: _index,
         items: [
-          BottomNavigationBarItem(title:Text("홈"),icon: Icon(Icons.home)),
-
-          BottomNavigationBarItem(title:Text("이용서비스"),icon: Icon(Icons.assignment)),
-
-          BottomNavigationBarItem(title:Text("내정보"),icon: Icon(Icons.account_circle)),
-
+          BottomNavigationBarItem(title: Text("홈"), icon: Icon(Icons.home)),
+          BottomNavigationBarItem(
+              title: Text("이용서비스"), icon: Icon(Icons.assignment)),
+          BottomNavigationBarItem(
+              title: Text("내정보"), icon: Icon(Icons.account_circle)),
         ],
       ),
     );
